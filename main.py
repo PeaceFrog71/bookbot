7a97e96570b7254c1038fa3ebfdbad3102e0892c
PeaceFrog71 <dafrog71@yahoo.com>
2024-07-02 16:22:47 -0600

    Updated main to include a file reader function and display the contents of frankenstien.txt to console

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 
 def main():
-    with open("./books/frankenstien.txt") as f:
-        file_contents = f.read()
+    book_path = "./books/frankenstien.txt"
+    file_contents = get_file_text(book_path)
     print(file_contents)
 
+
+def get_file_text(path):
+    with open("./books/frankenstien.txt") as f:
+        return f.read()
+    
 
 if __name__ == "__main__":
     main()
