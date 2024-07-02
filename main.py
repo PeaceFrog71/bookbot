b049c93f9d5c76c5ed63a6acdf651aa89bd48882
PeaceFrog71 <dafrog71@yahoo.com>
2024-07-02 15:48:11 -0600

    read in text and display to console.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+
+def main():
+    with open("./books/frankenstien.txt") as f:
+        file_contents = f.read()
+    print(file_contents)
+
+
+if __name__ == "__main__":
+    main()
